c4d90703f9e6d967d7d90502cb640c87c16dce64
Miguel Amaral <miguel.amaral04@gmail.com>
2017-04-03 21:50:53 +0100

    sign up without server call

--- app/src/main/java/cmu1617/andred/pt/locmess/MainService.java
+++ app/src/main/java/cmu1617/andred/pt/locmess/MainService.java
@@ -2,10 +2,12 @@ package cmu1617.andred.pt.locmess;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 
 public class MainService extends Service {
     public MainService() {
+
     }
 
     @Override

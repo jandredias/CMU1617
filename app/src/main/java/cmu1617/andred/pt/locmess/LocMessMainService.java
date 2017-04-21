9b2ff5e67cf94f34abbf96154a44fab4826768c5
Miguel Amaral <miguel.amaral04@gmail.com>
2017-04-22 00:26:58 +0100

    going windows

--- app/src/main/java/cmu1617/andred/pt/locmess/LocMessMainService.java
+++ app/src/main/java/cmu1617/andred/pt/locmess/LocMessMainService.java
@@ -2,17 +2,19 @@ package cmu1617.andred.pt.locmess;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import cmu1617.andred.pt.locmess.AsyncTasks.GetMessagesAsyncTask;
 
 import static android.R.attr.value;
 
-public class LocMessMainService extends Service {
+public class LocMessMainService extends Service implements GoogleApiClient.ConnectionCallbacks {
     private static final String TAG = "LocMessMainService";
     private GoogleApiClient mGoogleApiClient;
     protected static final int REQUEST_CHECK_SETTINGS = 0x1;
@@ -25,15 +27,15 @@ public class LocMessMainService extends Service {
         Log.d(TAG, "Service Started + " + value);
 
         // Create an instance of GoogleAPIClient.
-//        if (mGoogleApiClient == null) {
-//            mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                    .addConnectionCallbacks(this)
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
 //                    .addOnConnectionFailedListener(this)
-//                    .addApi(LocationServices.API)
-//                    .build();
-//
-//        }
-//        mGoogleApiClient.connect();
+                    .addApi(LocationServices.API)
+                    .build();
+
+        }
+        mGoogleApiClient.connect();
 
         task = new GetMessagesAsyncTask(new SQLDataStoreHelper(getBaseContext()));
         task.execute();
@@ -47,5 +49,19 @@ public class LocMessMainService extends Service {
         return null;
     }
 
+    @Override
+    public void onDestroy(){
+        mGoogleApiClient.disconnect();
+        super.onDestroy();
+    }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
 }
